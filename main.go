0bc6d3d59391a87c0ce3803105c09ea5999d1dc1
Sinta AT <sinta.tangkudung@dana.id>
2021-10-29 15:09:33 +0700

    Add endpoint that returns json response

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "net/http"
 )
@@ -11,6 +12,20 @@ func main() {
     http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
         textResponse := "Hello, world!"
         w.Write([]byte(textResponse))
+    })
+
+    http.HandleFunc("/foobar", func(w http.ResponseWriter, r *http.Request) {
+        mapResponse := map[string]string{
+            "foo": "bar",
+        }
+        responseBody, err := json.Marshal(mapResponse)
+        if err != nil {
+            w.WriteHeader(http.StatusInternalServerError)
+            return
+        }
+
+        w.Header().Add("Content-Type", "application/json")
+        w.Write(responseBody)
     })
 
     http.ListenAndServe(":80", nil)
