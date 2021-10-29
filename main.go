69c105f230d44993e13433de2caad0573b3ca9e1
Sinta AT <sinta.tangkudung@dana.id>
2021-10-29 15:02:22 +0700

    Initiate a HTTP server with one endpoint that returns hello world

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
     "fmt"
+    "net/http"
 )
 
 func main() {
-    fmt.Println("Hello, world!")
+    fmt.Println("Starting HTTP server...")
+
+    http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+        textResponse := "Hello, world!"
+        w.Write([]byte(textResponse))
+    })
+
+    http.ListenAndServe(":80", nil)
 }
